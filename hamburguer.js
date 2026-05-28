2a16a4d4c3b2446f7064ac008d1162d8c7704134
EduardoZs01 <eduardozambianco456@gmail.com>
2026-05-28 13:55:00 -0300

    inicioo_JavaScript_ git commit -m inicioo_JavaScript_ git commit -m inicioo_JavaScript_

--- hamburguer.js
+++ hamburguer.js
@@ -1,9 +1,13 @@
 alert (" Boas vindas a melho hamburgueria da Cidade, Eleita top 1 em 2026")
-alert(" lanches disponiveis, x-salada, x-cheedar e x-bacon ")
-alert("bebidas disponiveis, água, suco e refrigerante  ")
+alert(" lanches disponiveis, LORD MONSTER (pão brioche, 5 carnes de 129g de pura suculencia, misturas de queijos com queijo prato, mussarela, catupiry, cheddar ingles, bacon e onion rings) THE BEST (pão de brioche. 2 carnes de 100g, cheddar ingles e bacon) O KING (pão broche, 100g, queijo prato, chedar ingles, onion rings e bacon ) ),  ")
+alert("bebidas disponiveis, água, sucos (uva, laranja, limão), H2o, h2o limonede, refrigerante (coca, coca zero, sprit zero, sript, fanta uva e fanta laranja) ")
 
 let pedido = prompt("qual o seu pedido")
 console.log (pedido)
 let bebida = prompt("qual a sua bebida ")
 console.log (bebida)
 alert ("seus pedidos foram: " + pedido + " " + bebida )
+let compra = prompt ("qual a forma de pagamento")
+console.log (compra);
+alert ("sua transição foi aceita")
+alert ("seu pedido está a caminho, em média 20 min")
